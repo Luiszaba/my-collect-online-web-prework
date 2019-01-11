61286f224c05710a6e2e9fb69bdd9c96bc2646f8
Luis Zabala <luis.zabala076@gmail.com>
2019-01-11 00:54:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(array) 
 i = 0 
-newArray = []
 while i < array.length
-newArray << yield(array[i]).upcase
+my_collect(array) do {|newArray| newArray.upcase}
+newArray << yield(array[i])
 i = i + 0
 end
 newArray
